1a60434f511d4517918a830e1858d1a47f9cb468
Hiroyuki Tsuruda <mocchicc@me.com>
2014-11-08 13:34:12 +0900

    Optimized a Rspec code

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,10 @@
 require 'spec_helper'
 
+
 describe "StaticPages" do
+
+let(:base_title) { “Ruby on Rails Tutorial Sample App” }
+
   describe "Home Page" do
     it "should have the content 'Sample App'" do
     	visit '/static_pages/home'
@@ -8,7 +12,7 @@ describe "StaticPages" do
     end
     it "shoud have the title 'Home'" do
     	visit '/static_pages/home'
-    	expect(page).to have_title('Home')
+    	expect(page).to have_title('#{base_title} | Home')
     end
   end
 
@@ -19,7 +23,7 @@ describe "StaticPages" do
   	end
   	it "shoud have the title 'Help'" do
   		visit '/static_pages/help'
-  		expect(page).to have_title('Help')
+  		expect(page).to have_title('#{base_title} | Help')
   	end
   end
 
@@ -28,11 +32,9 @@ describe "StaticPages" do
   		visit '/static_pages/about'
   		expect(page).to have_content('About Us')
   	end
-  	it "shoud have the title 'about'" do
+  	it "shoud have the title 'About'" do
   		visit '/static_pages/about'
-  		expect(page).to have_title('About')
+  		expect(page).to have_title('#{base_title} | About')
   	end
   end
-
-
 end

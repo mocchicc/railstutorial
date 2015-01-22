6b8ba491c094bdbf0804acbe22da4fc4497b2991
Hiroyuki Tsuruda <mocchicc@me.com>
2015-01-22 10:39:18 +0900

    modified rooting

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,33 +7,33 @@ let(:base_title) { “Ruby on Rails Tutorial Sample App” }
 
   describe "Home Page" do
     it "should have the content 'Sample App'" do
-    	visit '/static_pages/home'
+    	visit home_path
     	expect(page).to have_content('Sample App')
     end
     it "shoud have the title 'Home'" do
-    	visit '/static_pages/home'
+    	visit home_path
     	expect(page).to have_title('#{base_title} | Home')
     end
   end
 
   describe "Help Page" do
   	it "shoud have the content 'Help'" do
-  		visit '/static_pages/help'
+  		visit help_path
   		expect(page).to have_content('Help')
   	end
   	it "shoud have the title 'Help'" do
-  		visit '/static_pages/help'
+  		visit help_path
   		expect(page).to have_title('#{base_title} | Help')
   	end
   end
 
   describe "About page" do
   	it "shoud have the content 'About Us'" do
-  		visit '/static_pages/about'
+  		visit about_path
   		expect(page).to have_content('About Us')
   	end
   	it "shoud have the title 'About'" do
-  		visit '/static_pages/about'
+  		visit about_path
   		expect(page).to have_title('#{base_title} | About')
   	end
   end
@@ -41,12 +41,12 @@ let(:base_title) { “Ruby on Rails Tutorial Sample App” }
   describe "Contact page" do
 
     it "should have the content 'Contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_content('Contact')
     end
 
     it "should have the title 'Contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
     end
   end
